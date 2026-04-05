c3b26291b5105e83cc2cea347335800e5f3e8127
ten_taken <fenrdark@gmail.com>
2026-04-05 14:44:55 -0400

    favicon. Introduce Banner and some assets.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
-
-// const navItems = [
-//   { title: 'Home', to: '/', icon: 'mdi-home-outline' },
-//   { title: 'About', to: '/about', icon: 'mdi-information-outline' },
-// ]
+import BannerNav from '@/components/BannerNav.vue'
 </script>
 
 <template>
   <v-app>
-    <v-app-bar border="sm" class="px-2" color="surface" elevation="0">
-      <template #prepend>
-        <v-avatar color="primary" rounded="lg" size="40" variant="tonal">
-          <img alt="Vue logo" src="@/assets/logo.svg" width="24" height="24" />
-        </v-avatar>
-      </template>
-
-      <v-app-bar-title class="font-weight-semibold"> The Mermaid Bar </v-app-bar-title>
-
-      <!-- <v-spacer /> -->
-
-      <!-- <v-btn
-        v-for="item in navItems"
-        :key="item.to"
-        :prepend-icon="item.icon"
-        :to="item.to"
-        class="ml-2"
-        rounded="pill"
-        variant="text"
-      >
-        {{ item.title }}
-      </v-btn> -->
-    </v-app-bar>
-
+    <banner-nav />
     <v-main>
       <v-container class="py-8">
         <RouterView />
---
+++ src/components/BannerNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar
+    border="sm"
+    class="px-2"
+    color="surface"
+    image="@/assets/appbeachbanner.jpg"
+    elevation="3"
+    density="prominent"
+  >
+    <template v-slot:image>
+      <v-img
+        :cover="true"
+        position="50% 25%"
+      ></v-img>
+    </template>
+
+    <template #prepend>
+      <img
+        alt="Vue logo"
+        src="@/assets/shell3.png"
+        width="24"
+        height="24"
+      />
+    </template>
+
+    <v-app-bar-title class="font-weight-semibold"> The Mermaid Bar </v-app-bar-title>
+
+    <!-- <v-spacer /> -->
+
+    <!-- <v-btn
+      v-for="item in navItems"
+      :key="item.to"
+      :prepend-icon="item.icon"
+      :to="item.to"
+      class="ml-2"
+      rounded="pill"
+      variant="text"
+    >
+      {{ item.title }}
+    </v-btn> -->
+  </v-app-bar>
+</template>
