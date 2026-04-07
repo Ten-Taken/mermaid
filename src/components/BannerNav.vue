9b5b1714afba129ebe6dcaaf09070610174d9a20
ten_taken <fenrdark@gmail.com>
2026-04-07 16:54:23 -0400

    Hook up some dynamic banner styling

--- src/components/BannerNav.vue
+++ src/components/BannerNav.vue
@@ -1,20 +1,96 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import homeBanner from '@/assets/appbeachbanner.jpg'
+import shopBanner from '@/assets/19201.jpg'
+import bookingBanner from '@/assets/58767.jpg'
+import locationBanner from '@/assets/30594.jpg'
+import contactBanner from '@/assets/7930358_445.jpg'
+
+const route = useRoute()
+
+const bannerConfig = computed(() => {
+  switch (route.name) {
+    case 'booking':
+      return {
+        title: 'Book Services',
+        image: bookingBanner,
+        imagePosition: '50% 65%',
+        appBarDensity: 'default',
+        homeIconColor: 'mermParaText',
+      }
+    case 'shop':
+      return {
+        title: 'Shop Jewelry & Hair Charms',
+        image: shopBanner,
+        imagePosition: '50% 65%',
+        appBarDensity: 'default',
+        homeIconColor: 'white',
+      }
+    case 'location':
+      return {
+        title: "Today's Location",
+        image: locationBanner,
+        imagePosition: '50% 75%',
+        appBarDensity: 'default',
+        homeIconColor: 'mermBannerBlue',
+      }
+    case 'contact':
+      return {
+        title: 'Contact Us',
+        image: contactBanner,
+        imagePosition: '50% 75%',
+        appBarDensity: 'default',
+        homeIconColor: 'mermBannerBlue',
+      }
+    default:
+      return {
+        title: 'The Mermaid Bar',
+        image: homeBanner,
+        imagePosition: '50% 25%',
+        appBarDensity: 'prominent',
+        homeIconColor: 'white',
+      }
+  }
+})
+</script>
+
 <template>
   <v-app-bar
     border="sm"
     class="px-2"
     color="surface"
-    image="@/assets/appbeachbanner.jpg"
+    :image="bannerConfig.image"
     elevation="3"
-    density="prominent"
+    :density="bannerConfig.appBarDensity"
   >
     <template v-slot:image>
       <v-img
         :cover="true"
-        position="50% 25%"
+        :position="bannerConfig.imagePosition"
       ></v-img>
     </template>
 
     <v-spacer />
+
+    <template
+      v-slot:append
+      v-if="bannerConfig.title != 'The Mermaid Bar'"
+    >
+      <v-tooltip text="Return Home">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            :to="{ name: 'home' }"
+            density="comfortable"
+            :color="bannerConfig.homeIconColor"
+            icon="mdi-home"
+            size="x-large"
+          ></v-btn>
+        </template>
+      </v-tooltip>
+    </template>
 
     <v-row class="align-self-center justify-center">
       <v-col cols="2">
@@ -29,7 +105,7 @@
         class="d-flex align-center"
         cols="auto"
       >
-        <v-app-bar-title class="banner-title"> The Mermaid Bar </v-app-bar-title>
+        <v-app-bar-title class="banner-title"> {{ bannerConfig.title }} </v-app-bar-title>
       </v-col>
       <v-col cols="1">
         <img
